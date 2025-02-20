a3d4151224f5c6512e3e7b00bdb0ccd0dd47ac15
Re Tyre <grexer@gmail.com>
2025-02-20 13:00:52 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,12 +21,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.s4
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.s4 \
-    libgptutils \
-    libz \
-    libcutils
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
